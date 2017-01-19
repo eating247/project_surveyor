27caf7c81619987c0de4d101bfe446628fa68760
tt <ttwang.303@gmail.com>
2017-01-19 02:55:08 -0500

    seed db

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ end
 
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'sass-rails', '>= 3.2'
+gem 'faker'
 
 group :development do
   gem "better_errors"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Destroying everything...'
+Option.destroy_all
+Question.destroy_all
+Survey.destroy_all
+puts 'DONE'
+
+puts 'Creating surveys...'
+3.times do
+  Survey.create(
+    title: Faker::LordOfTheRings.character,
+    description: Faker::Hipster.sentence)
+end
+puts 'DONE'
+
+puts 'Creating questions...'
+Survey.all.each do |survey|
+  rand(1..5).times do
+    Question.create(
+      content: Faker::Hipster.sentence,
+      question_type: 'Multiple Choice',
+      survey_id: survey.id,
+      required: false,
+      num_options: 2,
+      one_selection: true
+      )
+  end
+end
+puts "DONE"
+
+puts 'Creating options...'
+Question.all.each do |question|
+  question.num_options.times do
+    Option.create(
+      question_id: question.id,
+      content: Faker::Pokemon.location
+      )
+  end
+end
+puts 'DONE'
+
